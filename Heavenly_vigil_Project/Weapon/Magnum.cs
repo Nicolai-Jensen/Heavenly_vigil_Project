12f7dbe3e4d3f751d83745d5a79173e1ce113cc0
Nicolai-Jensen <nicolaijensen11@gmail.com>
2022-11-03 10:06:53 +0100

    very few cchanegs

--- Heavenly_vigil_Project/Weapon/Magnum.cs
+++ Heavenly_vigil_Project/Weapon/Magnum.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using SharpDX.Direct3D9;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +12,41 @@ namespace Heavenly_vigil_Project
 {
     internal class Magnum : Weapon
     {
+        // -----FIELDS-----
+
+        // -----CONSTRUCTORS-----
+        public Magnum(Texture2D sprite, Vector2 position)
+        {
+            objectSprites = new Texture2D[1];
+            objectSprites[0] = sprite;
+            this.position = position;
+            scale = 0.8f;
+
+            velocity += new Vector2(0, -1);
+            speed = 1500;
+        }
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(objectSprites[0], position, null, Color.White, 0, origin, scale, SpriteEffects.None, 0);
+        }
+
+        public override void LoadContent(ContentManager content)
+        {
+            origin = new Vector2(objectSprites[0].Width / 2, objectSprites[0].Height / 2);
+        }
+
+        public override void OnCollision(GameObject other)
+        {
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            Move(gameTime);
+        }
+        public override void Attack(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
