4e04cd5f5ae51951fbded945efb5270f0855c1db
MarcusN <MarcusNN@outlook.dk>
2022-11-07 12:04:51 +0100

    ManaBar
    ManaBar UI

--- Heavenly_vigil_Project/Interface/UserInterface.cs
+++ Heavenly_vigil_Project/Interface/UserInterface.cs
@@ -17,10 +17,12 @@ namespace Heavenly_vigil_Project
         private Rectangle blueRectangle;
         //Rectangle for the xp bar background
         private Rectangle xpBlackrectangle;
+        //Rectangle for the mana bar
+        private Rectangle manaRectangle;
         private SpriteFont gameFont;
         private SpriteFont timerFont;
-        
-        
+
+
 
         //Properties
 
@@ -36,6 +38,7 @@ namespace Heavenly_vigil_Project
             greenRectangle = new Rectangle(10, 10, Player.Health * 2, 20);
             blackRectangle = new Rectangle(0, 0, 220, 40);
             blueRectangle = new Rectangle(0, 0, ExperiencePoints.ExpPercentage, 20);
+            manaRectangle = new Rectangle(0, 0, Player.Mana, 20);
             xpBlackrectangle = new Rectangle(0, 0, 1900, 40);
             gameFont = content.Load<SpriteFont>("GameFont");
             timerFont = content.Load<SpriteFont>("TimerFont");
@@ -47,9 +50,10 @@ namespace Heavenly_vigil_Project
             TimeManager.UpdateClock(gameTime);
             ExperiencePoints.CheckForLevelUp(gameTime);
             blueRectangle.Width = (int)(ExperiencePoints.ExpPercentage * 18.75);
-           greenRectangle.Width = Player.Health * 2;
+            greenRectangle.Width = Player.Health * 2;
+            manaRectangle.Width = Player.Mana * 2;
 
-            
+
         }
 
         public override void Draw(SpriteBatch spriteBatch)
@@ -58,12 +62,13 @@ namespace Heavenly_vigil_Project
             spriteBatch.Draw(objectSprites[0], new Vector2(20, 20), greenRectangle, Color.White);
             spriteBatch.Draw(objectSprites[1], new Vector2(10, 1000), xpBlackrectangle, Color.White);
             spriteBatch.Draw(objectSprites[2], new Vector2(20, 1010), blueRectangle, Color.White);
+            spriteBatch.Draw(objectSprites[2], new Vector2(20, 60), manaRectangle, Color.White);
             spriteBatch.End();
             spriteBatch.Begin();
             spriteBatch.DrawString(gameFont, $"Lvl. {ExperiencePoints.PlayerLevel}", new Vector2(950, 1009), Color.White);
             spriteBatch.End();
             spriteBatch.Begin();
             spriteBatch.DrawString(timerFont, $"{TimeManager.timerMinutes} : {TimeManager.timerSeconds}", new Vector2(GameWorld.ScreenSize.X / 2, 10), Color.White);
-            }
         }
     }
+}
